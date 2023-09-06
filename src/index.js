9c60ff9a438e82966ff2a0ab2e189157b701aa62
edteves <edwinjrteves29@gmail.com>
2023-09-07 06:16:53 +0800

    new codes updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './js/buy-now-modal.js';
 import './js/form-buy-now.js';
-import './js/mobile-menu.js';
+
 import './js/subscribe.js';
 import './js/swiper.js';
 import './js/mobile-menu.js';
