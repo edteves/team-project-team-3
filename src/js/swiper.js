66e41292c340193061e0c7f87463a23a34c22df3
edteves <edwinjrteves29@gmail.com>
2023-09-09 15:32:43 +0800

    final

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,7 +1,37 @@
+// var swiper = new Swiper('.swiper', {
+//   slidesPerView: 1,
+//   spaceBetween: 0,
+//   keyboard: {
+//     enabled: true,
+//   },
+//   breakpoints: {
+//     375: {
+//       slidesPerView: 1,
+//       spaceBetween: 18,
+//     },
+//     // when window width is >= 768px
+//     768: {
+//       slidesPerView: 2.3,
+//       spaceBetween: 18,
+//     },
+//     // when window width is >= 1200px
+//     1200: {
+//       slidesPerView: 4,
+//       spaceBetween: 18,
+//     },
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// });
 var swiper = new Swiper('.swiper', {
   slidesPerView: 1,
-  spaceBetween: 0,
-  loop: true,
+  spaceBetween: 18,
+
+  keyboard: {
+    enabled: true,
+  },
   breakpoints: {
     375: {
       slidesPerView: 1,
@@ -9,13 +39,42 @@ var swiper = new Swiper('.swiper', {
     },
     // when window width is >= 768px
     768: {
-      slidesPerView: 2.36,
+      slidesPerView: 2.5,
       spaceBetween: 18,
     },
     // when window width is >= 1200px
     1200: {
       slidesPerView: 4,
-      spaceBetween: 0,
+      spaceBetween: 18,
+    },
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper('.swiper-review', {
+  slidesPerView: 1,
+  spaceBetween: 18,
+
+  keyboard: {
+    enabled: true,
+  },
+  breakpoints: {
+    // 375: {
+    //   slidesPerView: 1,
+    //   spaceBetween: 0,
+    // },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 35,
     },
   },
   pagination: {
